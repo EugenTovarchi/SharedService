2ed28152dec696c6eda2f9b6045bcf411b440ff8
Eugen T <rovanphone@gmail.com>
2026-02-28 12:21:51 +0300

    add EndpointResult, ErrorResult.cs,SuccessResult

---
+++ SharedService.Framework/EndpointSettings/ErrorResult.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using SharedService.SharedKernel;
+
+namespace SharedService.Framework.EndpointSettings;
+
+public class ErrorResult : IResult
+{
+    private readonly Error _error;
+
+    public ErrorResult(Error error)
+    {
+        _error = error;
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        int statusCode = GetStatusCodeFromErrorType(_error.Type);
+
+        var envelope = Envelope.Error(_error);
+        httpContext.Response.StatusCode = statusCode;
+
+        return httpContext.Response.WriteAsJsonAsync(envelope);
+    }
+
+    private int GetStatusCodeFromErrorType(ErrorType? errorType) =>
+        errorType switch
+        {
+            ErrorType.VALIDATION => StatusCodes.Status400BadRequest,
+            ErrorType.NOT_FOUND => StatusCodes.Status404NotFound,
+            ErrorType.CONFLICT => StatusCodes.Status409Conflict,
+            _ => StatusCodes.Status500InternalServerError
+        };
+}
