988f284f7932d0cd1d93d3bdcd4a3b0da478ea7a
Eugen T <rovanphone@gmail.com>
2026-04-17 17:30:13 +0300

    add records for messaging: FileUploaded, FileDeleted.cs add WolverineErrorHandleExtensions.cs add FileEventsRouting.cs

---
+++ SharedService.SharedKernel/Messaging/Files/FileEventsRouting.cs
@@ -0,0 +1,23 @@
+﻿namespace SharedService.SharedKernel.Messaging.Files;
+
+public static class FileEventsRouting
+{
+    public const string EXCHANGE = "file.events";
+    public const string ALL_DIRECTORY_EVENTS = "directory.*.*";
+    public const string ALL_FILE_UPLOADED = "file.uploaded.#";
+    public const string ALL_FILE_DELETED = "file.deleted.#";
+
+    public static class RoutingKeys
+    {
+        // file.deleted.photo.department (binding to queue: directory.file.events)
+        public static string FileDeleted(string assetType, string entityType)
+        {
+            return $"file.deleted.{assetType.ToLowerInvariant()}.{entityType.ToLowerInvariant()}";
+        }
+
+        public static string FileUploaded(string assetType, string entityType)
+        {
+            return $"file.uploaded.{assetType.ToLowerInvariant()}.{entityType.ToLowerInvariant()}";
+        }
+    }
+}
---
+++ SharedService.SharedKernel/Messaging/Files/WolverineErrorHandleExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Npgsql;
+using SharedService.SharedKernel.Exceptions;
+using Wolverine;
+using Wolverine.ErrorHandling;
+
+namespace SharedService.SharedKernel.Messaging.Files;
+
+public static class WolverineErrorHandleExtensions
+{
+    public static void ConfigureStandardErrorPolicies(this WolverineOptions opts)
+    {
+        opts.Policies.OnException<NpgsqlException>(ex => ex.IsTransient)
+            .RetryWithCooldown(TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(3))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(10));
+
+        opts.Policies.OnException<TimeoutException>()
+            .RetryWithCooldown(TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(3))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(15));
+
+        opts.Policies.OnException<HttpRequestException>()
+            .RetryWithCooldown(TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(3))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(15));
+
+        opts.Policies.OnException<TaskCanceledException>(ex => ex.InnerException is TimeoutException)
+            .RetryWithCooldown(TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(3))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(15));
+
+        opts.Policies.OnException<IOException>()
+            .ScheduleRetry(TimeSpan.FromSeconds(1))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(5));
+
+        opts.Policies.OnException<ValidationException>().MoveToErrorQueue();
+        opts.Policies.OnException<ArgumentException>().MoveToErrorQueue();
+        opts.Policies.OnException<InvalidCastException>().MoveToErrorQueue();
+        opts.Policies.OnException<FailureException>().MoveToErrorQueue();
+        opts.Policies.OnException<NotFoundException>().MoveToErrorQueue();
+        opts.Policies.OnException<ConflictException>().MoveToErrorQueue();
+
+        opts.Policies.OnException<Exception>()
+            .RetryWithCooldown(TimeSpan.FromMilliseconds(100), TimeSpan.FromMilliseconds(250))
+            .Then.ScheduleRetry(TimeSpan.FromSeconds(5))
+            .Then.MoveToErrorQueue();
+    }
+}
