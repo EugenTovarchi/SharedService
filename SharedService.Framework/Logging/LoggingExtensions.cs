4455897ec50a1c0f6553352f40e5c8f7a27c33d6
Eugen T <rovanphone@gmail.com>
2026-04-26 19:35:09 +0300

    add Serilog.Sinks.Grafana.Loki

--- SharedService.Framework/Logging/LoggingExtensions.cs
+++ SharedService.Framework/Logging/LoggingExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Core;
 using Serilog.Exceptions;
+using Serilog.Sinks.Grafana.Loki;
 
 namespace SharedService.Framework.Logging;
 
@@ -13,14 +14,24 @@ public static class LoggingExtensions
         IConfiguration configuration,
         string serviceName = "Service")
     {
-        if (Log.Logger == null || !(Log.Logger is Logger))
+        if (Log.Logger == null || Log.Logger is not Logger)
         {
-            Log.Logger = new LoggerConfiguration()
+            string? lokiUrl = configuration["Logging:Loki:Url"];
+
+            var loggerConfiguration = new LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
                 .Enrich.FromLogContext()
                 .Enrich.WithExceptionDetails()
-                .Enrich.WithProperty("ServiceName", serviceName)
-                .CreateLogger();
+                .Enrich.WithProperty("ServiceName", serviceName);
+
+            if (!string.IsNullOrWhiteSpace(lokiUrl))
+            {
+                loggerConfiguration.WriteTo.GrafanaLoki(
+                    lokiUrl,
+                    labels: new[] { new LokiLabel { Key = "service", Value = serviceName } });
+            }
+
+            Log.Logger = loggerConfiguration.CreateLogger();
         }
 
         services.AddSerilog();
